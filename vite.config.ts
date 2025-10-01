cae53b6e3f3386656703517a4bc5cb764dd8d049
SelfLearningMarco <marcojustforlearning@gmail.com>
2025-10-01 22:08:42 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mahjong-list/",
+  build: {
+    chunkSizeWarningLimit: 1500, // or any larger value
+  },
 })
